4741ae26f567ca914394de3424e752931bebb8b0
NicolasLewin <nicolas.lewin@outlook.com>
2024-11-09 16:04:24 +0100

    deleted key and added env access

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request: any) {
     const { searchParams } = new URL(request.url)
@@ -8,9 +8,9 @@ export async function GET(request: any) {
 
     let url = "";
     if(address) {
-        url = "https://api.openweathermap.org/data/2.5/weather?q=" + address + "&appid=" + "060bfd7cdbd51abe374082b0ac7aabe6"
+        url = "https://api.openweathermap.org/data/2.5/weather?q=" + address + "&appid=" + process.env.WEATHER_API_KEY
     } else {
-        url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=060bfd7cdbd51abe374082b0ac7aabe6`;
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=` + process.env.WEATHER_API_KEY;
     }
 
     const res = await fetch(url);
